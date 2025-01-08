8f9ecadae69a8a4c286f43bdabaa480c9acfdf4b
arttttt <artem-bambalov@yandex.ru>
2025-01-08 02:50:22 +0200

    updates

--- app/src/main/java/com/arttttt/oldphoneanimation/MainActivity.kt
+++ app/src/main/java/com/arttttt/oldphoneanimation/MainActivity.kt
@@ -4,15 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.arttttt.oldphoneanimation.ui.theme.OldPhoneAnimationTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,9 +37,41 @@ class MainActivity : ComponentActivity() {
                             .padding(innerPadding),
                         contentAlignment = Alignment.Center,
                     ) {
-                        OldPhone(
-                            modifier = Modifier.padding(innerPadding),
-                        )
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.spacedBy(8.dp),
+                        ) {
+                            var phoneNumber by remember {
+                                mutableStateOf("")
+                            }
+
+                            Text(
+                                modifier = Modifier
+                                    .align(Alignment.Start)
+                                    .padding(
+                                        horizontal = 16.dp,
+                                    ),
+                                text = "Phone number: $phoneNumber",
+                                style = MaterialTheme.typography.headlineLarge,
+                            )
+
+                            OldPhone(
+                                modifier = Modifier.padding(innerPadding),
+                                onDigitSelected = { digit ->
+                                    phoneNumber += digit
+                                },
+                            )
+
+                            Button(
+                                onClick = {
+                                    phoneNumber = ""
+                                }
+                            ) {
+                                Text(
+                                    text = "Clear phone number",
+                                )
+                            }
+                        }
                     }
                 }
             }
--- app/src/main/java/com/arttttt/oldphoneanimation/OldPhone.kt
+++ app/src/main/java/com/arttttt/oldphoneanimation/OldPhone.kt
@@ -15,8 +15,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.PathFillType
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.input.pointer.pointerInput
@@ -56,6 +58,7 @@ private val digits = listOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "0")
 @Composable
 fun OldPhone(
     modifier: Modifier,
+    onDigitSelected: (String) -> Unit,
 ) {
     var rotationAngle by remember { mutableFloatStateOf(0f) }
     val textMeasurer = rememberTextMeasurer()
@@ -67,6 +70,19 @@ fun OldPhone(
     val animatedRotationAngle by animateFloatAsState(
         targetValue = rotationAngle
     )
+
+    val texts = remember {
+        digits.map { digit ->
+            textMeasurer.measure(
+                text = digit,
+                style = typography,
+            )
+        }
+    }
+
+    val baseColor = Color(0xFFF5F2EA)
+    val coverColor = Color(0xFF2B2522).copy(alpha = 0.92f)
+    val centerAndFrameColor = Color(0xFFE5E0D5)
 
     Canvas(
         modifier = modifier
@@ -86,6 +102,7 @@ fun OldPhone(
 
                     if (digitHit != -1) {
                         var currentAngle = calculateAngle(down.position, size.center)
+                        val maxRotation = (digitHit + 1) * 27f
 
                         do {
                             val event = awaitPointerEvent()
@@ -100,8 +117,6 @@ fun OldPhone(
 
                             val targetRotation = rotationAngle + deltaAngle
 
-                            val maxRotation = (digitHit + 1) * 27f
-
                             if (rotationAngle <= maxRotation) {
                                 rotationAngle = targetRotation.coerceIn(0f, maxRotation)
                             }
@@ -109,6 +124,11 @@ fun OldPhone(
                             currentAngle = newAngle
                             event.changes.first().consume()
                         } while (event.changes.any { it.pressed })
+
+                        val isDigitFullySelected = rotationAngle >= maxRotation * 0.9f
+                        if (isDigitFullySelected) {
+                            onDigitSelected(digits[digitHit])
+                        }
 
                         rotationAngle = 0f
                         activeDigitIndex = -1
@@ -121,26 +141,40 @@ fun OldPhone(
         val digitSize = 64.dp.toPx()
         val angleStep = -270f / digits.size
 
-        digits.forEachIndexed { index, _ ->
+        drawCircle(
+            color = baseColor,
+        )
+
+        digits.forEachIndexed { index, digit ->
             val angleInDegrees = index * angleStep - 45
             val angleInRadians = Math.toRadians(angleInDegrees.toDouble())
 
-            val x = center.x + radius * cos(angleInRadians).toFloat()
-            val y = center.y + radius * sin(angleInRadians).toFloat()
+            digitBoundsList[index].update(
+                center.x + radius * cos(angleInRadians).toFloat(),
+                center.y + radius * sin(angleInRadians).toFloat(),
+                digitSize,
+            )
 
-            digitBoundsList[index].update(x, y, digitSize)
+            val bounds = digitBoundsList[index]
+            val x = (bounds.left + bounds.right) / 2
+            val y = (bounds.top + bounds.bottom) / 2
+
+            val textLayoutResult: TextLayoutResult = texts[index]
+
+            drawText(
+                textLayoutResult = textLayoutResult,
+                topLeft = Offset(
+                    x - textLayoutResult.size.width / 2f,
+                    y - textLayoutResult.size.height / 2f,
+                ),
+            )
         }
 
         rotate(animatedRotationAngle) {
-
-            drawCircle(
-                color = Color.White,
-            )
-
             val centerCircleRadius = size.width / 3.21f
 
             drawCircle(
-                color = Color.LightGray,
+                color = centerAndFrameColor,
                 radius = centerCircleRadius,
                 center = center
             )
@@ -162,38 +196,48 @@ fun OldPhone(
                 )
             }
 
-            digits.forEachIndexed { index, digit ->
-                val bounds = digitBoundsList[index]
-                val x = (bounds.left + bounds.right) / 2
-                val y = (bounds.top + bounds.bottom) / 2
-
-                val textLayoutResult: TextLayoutResult = textMeasurer.measure(
-                    text = digit,
-                    style = typography,
+            val diskPath = Path().apply {
+                addOval(
+                    Rect(
+                        center = center,
+                        radius = size.width / 2f
+                    )
                 )
 
-                drawText(
-                    textLayoutResult = textLayoutResult,
-                    topLeft = Offset(
-                        x - textLayoutResult.size.width / 2f,
-                        y - textLayoutResult.size.height / 2f,
-                    ),
-                )
+                digits.forEachIndexed { index, _ ->
+                    val angleInDegrees = index * angleStep - 45
+                    val angleInRadians = Math.toRadians(angleInDegrees.toDouble())
 
-                val isActive = index == activeDigitIndex
+                    val x = center.x + radius * cos(angleInRadians).toFloat()
+                    val y = center.y + radius * sin(angleInRadians).toFloat()
 
-                if (isActive) {
-                    drawCircle(
-                        color = Color.Blue.copy(alpha = 0.3f),
-                        radius = digitSize / 1.8f,
-                        center = Offset(x, y)
+                    addOval(
+                        Rect(
+                            center = Offset(x, y),
+                            radius = digitSize / 2f
+                        )
                     )
                 }
+
+                addOval(
+                    Rect(
+                        center = center,
+                        radius = centerCircleRadius
+                    )
+                )
+
+                fillType = PathFillType.EvenOdd
             }
+
+            drawPath(
+                path = diskPath,
+                color = coverColor,
+            )
+
         }
 
         drawCircle(
-            color = Color.LightGray,
+            color = centerAndFrameColor,
             radius = size.width / 2f,
             center = center,
             style = Stroke(
@@ -201,18 +245,15 @@ fun OldPhone(
             ),
         )
 
-        // Добавляем стоппер в виде трапеции
         val stopperAngle = 0.0
-        val stopperOuterDistance = size.width / 2f // внешний радиус, на границе диска
-        val stopperInnerDistance = size.width / 2f - 48.dp.toPx() // увеличили глубину стоппера
+        val stopperOuterDistance = size.width / 2f
+        val stopperInnerDistance = size.width / 2f - 48.dp.toPx()
 
-        // Точки для внешней (широкой) части трапеции
         val outerLeftX = center.x + stopperOuterDistance * cos(Math.toRadians(stopperAngle - 5).toDouble()).toFloat()
         val outerLeftY = center.y + stopperOuterDistance * sin(Math.toRadians(stopperAngle - 5).toDouble()).toFloat()
         val outerRightX = center.x + stopperOuterDistance * cos(Math.toRadians(stopperAngle + 5).toDouble()).toFloat()
         val outerRightY = center.y + stopperOuterDistance * sin(Math.toRadians(stopperAngle + 5).toDouble()).toFloat()
 
-        // Точки для внутренней (узкой) части трапеции
         val innerLeftX = center.x + stopperInnerDistance * cos(Math.toRadians(stopperAngle - 2.5).toDouble()).toFloat()
         val innerLeftY = center.y + stopperInnerDistance * sin(Math.toRadians(stopperAngle - 2.5).toDouble()).toFloat()
         val innerRightX = center.x + stopperInnerDistance * cos(Math.toRadians(stopperAngle + 2.5).toDouble()).toFloat()
@@ -226,7 +267,7 @@ fun OldPhone(
                 lineTo(innerLeftX, innerLeftY)
                 close()
             },
-            color = Color.LightGray
+            color = centerAndFrameColor,
         )
     }
 }
@@ -259,7 +300,8 @@ fun Offset.rotate(
 private fun OldPhonePreview() {
     OldPhoneAnimationTheme {
         OldPhone(
-            modifier = Modifier
+            modifier = Modifier,
+            onDigitSelected = {},
         )
     }
 }
